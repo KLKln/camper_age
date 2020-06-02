3ac44d4268736fd0256c2f6fd0b655c031c03b82
Kelly Klein <kmklein2@dmacc.edu>
2020-06-02 14:38:39 -0500

    Update camper_age_input.py

--- camper_age_input.py
+++ camper_age_input.py
@@ -2,18 +2,16 @@
 Program: camper_age_input.py
 Author: Kelly Klein
 Last date modified: 6/2/2020
-this program will...
+this program will take input from user for the age
+    of a in years child and return age in months
 """
 
 
-def convert_to_months(x=input('Enter age of child: ')):
-
-    age = int(x)
-    age_in_months = age * 12
-    print("The child is", age_in_months, "months old")
+def convert_to_months(years):
+    return int(years) * 12
 
 
-convert_to_months()
-
-
-#if __name__ == '__main__':
+if __name__ == '__main__':
+    years = input('Enter age of child: ')
+    months = convert_to_months(years)
+    print("The child is", months, "months old")
